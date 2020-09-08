841e544cdb7db7d08c75d94f78b27fb751e18a62
infomuscle <infomuscle10@gmail.com>
2020-09-08 15:11:26 +0900

    URL Settings

--- crawler/crawler/urls.py
+++ crawler/crawler/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework import routers
+from gateway import views
+
+app_name = 'gateway'
+
+router = routers.DefaultRouter()
+router.register(r'gateway', views.Gateway)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    url('api-auth/', include('rest_framework.urls')),
+    url(r'^$', include(router.urls))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
