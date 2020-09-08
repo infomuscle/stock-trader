92e8738b986a17b631117b4ae1c82f048ea9373c
infomuscle <infomuscle10@gmail.com>
2020-09-09 01:40:17 +0900

    Test Connection to Crawler API

--- front/main/views.py
+++ front/main/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+import requests
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
     return HttpResponse("Main Screen!")
 
+
 def test(request):
     return HttpResponse("Test!")
+
+
+def current(request):
+    current_price = requests.get("http://127.0.0.1:8081/api/current")
+    return HttpResponse(current_price)
+
+
+def daily(request):
+    daily_price = requests.get("http://127.0.0.1:8081/api/daily")
+    return HttpResponse(daily_price)
