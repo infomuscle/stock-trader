e77f41a97da1097cdc9e7c82bc88301bb0621657
infomuscle <infomuscle10@gmail.com>
2020-09-10 02:27:58 +0900

    Connected Basic Django Templates

--- front/main/urls.py
+++ front/main/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('test', views.test),
     path('current', views.current),
     path('daily', views.daily)
 ]
--- front/main/views.py
+++ front/main/views.py
@@ -1,20 +1,23 @@
 import requests
+from main import constants as consts
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Main Screen!")
-
-
-def test(request):
-    return HttpResponse("Test!")
+    context = {}
+    return render(request, 'main/index.html', context)
 
 
 def current(request):
-    current_price = requests.get("http://127.0.0.1:8081/api/current")
+    url = consts.URL_BODY_CRAWLER + "/api/current"
+    url += '?code=' + consts.CODE_SAMSUNG_ELECTRONICS
+    current_price = requests.get(url)
     return HttpResponse(current_price)
 
 
 def daily(request):
-    daily_price = requests.get("http://127.0.0.1:8081/api/daily")
+    url = consts.URL_BODY_CRAWLER + "/api/daily"
+    url += '?code=' + consts.CODE_SAMSUNG_ELECTRONICS
+    daily_price = requests.get(url)
     return HttpResponse(daily_price)
