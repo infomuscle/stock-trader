fd92406f1ca4d64f9e249314803bb62b0d4affad
infomuscle <infomuscle10@gmail.com>
2020-09-10 17:13:24 +0900

    Added PER & PERS Views

--- front/main/views.py
+++ front/main/views.py
@@ -21,3 +21,16 @@ def daily(request):
     url += '?code=' + consts.CODE_SAMSUNG_ELECTRONICS
     daily_price = requests.get(url)
     return HttpResponse(daily_price)
+
+
+def per(request):
+    url = consts.URL_BODY_CRAWLER + "/api/per"
+    url += '?code=' + consts.CODE_SAMSUNG_ELECTRONICS
+    per = requests.get(url)
+    return HttpResponse(per)
+
+
+def pers(request):
+    url = consts.URL_BODY_CRAWLER + "/api/pers"
+    pers = requests.get(url)
+    return HttpResponse(pers)
