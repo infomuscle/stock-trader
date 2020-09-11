35a131f8a97c5a90f34da1e2c555a83bbf52a26f
infomuscle <infomuscle10@gmail.com>
2020-09-12 02:34:08 +0900

    Hide Secret Key

--- front/front/settings.py
+++ front/front/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rm%pc7gs(x+j(qwwn0#vok8ujo#t5g-f@1hx1#o6#i$y76wce-'
+SECRET_KEY = 'MUSTCREATENEWSECRETKEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
