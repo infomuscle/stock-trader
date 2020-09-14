055f977f4fa12bae98766bc992526aff08675708
infomuscle <infomuscle10@gmail.com>
2020-09-15 00:37:58 +0900

    Generated Basic Page Skeleton

--- front/main/urls.py
+++ front/main/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path('', views.index),
-    path('current', views.current, name='current'),
-    path('daily', views.daily, name='daily'),
-    path('per', views.per, name='per'),
-    path('pers', views.pers, name='pers')
+    path("", views.index),
+    path("current", views.current, name="current"),
+    path("daily", views.daily, name="daily"),
+    path("pers", views.per, name="per"),
+    path("summary", views.summary, name="summary")
 ]
