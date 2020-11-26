de936c8572382d32482d43efafea826c541550b0
infomuscle <infomuscle10@gmail.com>
2020-11-27 01:08:30 +0900

    Applied Wrapper Extends and Build Crawl Page Template

--- front/main/views.py
+++ front/main/views.py
@@ -11,6 +11,11 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 
+def crawl(request):
+    context = {}
+    return render(request, 'main/crawl.html', context)
+
+
 def current(request):
     req_json = request.GET.dict()
     code = req_json.get("code")
@@ -34,31 +39,32 @@ def daily(request):
     return render(request, 'main/daily.html', context)
 
 
-def per_deprecated(request):
-    req_json = request.GET.dict()
-    code = req_json.get("code")
-    url = consts.URL_BODY_CRAWLER + "/api/per"
-    url += '?code=' + code
-    per = requests.get(url)
-    return HttpResponse(per)
+# def per_deprecated(request):
+#     req_json = request.GET.dict()
+#     code = req_json.get("code")
+#     url = consts.URL_BODY_CRAWLER + "/api/per"
+#     url += '?code=' + code
+#     per = requests.get(url)
+#     return HttpResponse(per)
 
 
-def per(request):
-    url_tickers = consts.URL_BODY_CRAWLER + "/api/companies/list"
-    res_tickers = requests.get(url_tickers)
-    tickers = json.loads(res_tickers.content)
-
-    pers = dict()
-    for i, ticker in enumerate(tickers):
-        if i == 10:
-            break
-        url_per = consts.URL_BODY_CRAWLER + "/api/per"
-        url_per += "?code=" + ticker["code"]
-        res_per = requests.get(url_per)
-        pers[ticker["name"]] = res_per.text
-
-    context = {}
-    return render(request, 'main/per.html', context)
+#
+# def per(request):
+#     url_tickers = consts.URL_BODY_CRAWLER + "/api/companies/list"
+#     res_tickers = requests.get(url_tickers)
+#     tickers = json.loads(res_tickers.content)
+#
+#     pers = dict()
+#     for i, ticker in enumerate(tickers):
+#         if i == 10:
+#             break
+#         url_per = consts.URL_BODY_CRAWLER + "/api/per"
+#         url_per += "?code=" + ticker["code"]
+#         res_per = requests.get(url_per)
+#         pers[ticker["name"]] = res_per.text
+#
+#     context = {}
+#     return render(request, 'main/per.html', context)
 
 
 def summary(request):
