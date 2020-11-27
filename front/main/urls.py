3122dcc1456c6ad376c99c34f73cd7be0277ed39
infomuscle <infomuscle10@gmail.com>
2020-11-27 14:08:58 +0900

    Updated Menu Structure

--- front/main/urls.py
+++ front/main/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("", views.index),
     path("crawl", views.crawl, name="crawl"),
     path("companies", views.companies, name="companies"),
-    path("current", views.current, name="current"),
+    path("price", views.price, name="price"),
+    path("indicator", views.indicator, name="indicator"),
     path("daily", views.daily, name="daily"),
-    # path("pers", views.per, name="per"),
     path("summary", views.summary, name="summary")
 ]
--- front/main/views.py
+++ front/main/views.py
@@ -24,16 +24,14 @@ def companies(request):
     return render(request, 'main/companies.html', context)
 
 
-def current(request):
-    req_json = request.GET.dict()
-    code = req_json.get("code")
-    url = consts.URL_BODY_CRAWLER + "/api/current"
-    # url += '?code=' + code
-    url += '?code=' + consts.CODE_SAMSUNG_ELECTRONICS
-    current_price = requests.get(url)
-
+def price(request):
     context = {}
-    return render(request, 'main/current.html', context)
+    return render(request, 'main/price.html', context)
+
+
+def indicator(request):
+    context = {}
+    return render(request, 'main/indicator.html', context)
 
 
 def daily(request):
@@ -45,34 +43,6 @@ def daily(request):
 
     context = {}
     return render(request, 'main/daily.html', context)
-
-
-# def per_deprecated(request):
-#     req_json = request.GET.dict()
-#     code = req_json.get("code")
-#     url = consts.URL_BODY_CRAWLER + "/api/per"
-#     url += '?code=' + code
-#     per = requests.get(url)
-#     return HttpResponse(per)
-
-
-#
-# def per(request):
-#     url_tickers = consts.URL_BODY_CRAWLER + "/api/companies/list"
-#     res_tickers = requests.get(url_tickers)
-#     tickers = json.loads(res_tickers.content)
-#
-#     pers = dict()
-#     for i, ticker in enumerate(tickers):
-#         if i == 10:
-#             break
-#         url_per = consts.URL_BODY_CRAWLER + "/api/per"
-#         url_per += "?code=" + ticker["code"]
-#         res_per = requests.get(url_per)
-#         pers[ticker["name"]] = res_per.text
-#
-#     context = {}
-#     return render(request, 'main/per.html', context)
 
 
 def summary(request):
