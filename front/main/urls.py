3244f128feaf1e3e07dfd20af33ee105a25f3cbb
infomuscle <infomuscle10@gmail.com>
2020-11-27 15:14:11 +0900

    Arranged URLs and Views

--- front/main/urls.py
+++ front/main/urls.py
@@ -7,10 +7,9 @@ app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("companies", views.companies, name="companies"),
+    path("summary", views.summary, name="summary"),
     path("price", views.price, name="price"),
     path("indicator", views.indicator, name="indicator"),
     path("crawl", views.crawl, name="crawl"),
-    path("trade", views.trade, name="trade"),
-    path("daily", views.daily, name="daily"),
-    path("summary", views.summary, name="summary")
+    path("trade", views.trade, name="trade")
 ]
--- front/main/views.py
+++ front/main/views.py
@@ -1,23 +1,11 @@
-import requests
 from django.shortcuts import render
 
-from main import constants as consts
 from main.models import *
 
 
 def index(request):
     context = {}
     return render(request, 'main/index.html', context)
-
-
-def crawl(request):
-    context = {}
-    return render(request, 'main/crawl.html', context)
-
-
-def trade(request):
-    context = {}
-    return render(request, 'main/trade.html', context)
 
 
 def companies(request):
@@ -27,6 +15,11 @@ def companies(request):
     context["companies"] = companies
 
     return render(request, 'main/companies.html', context)
+
+
+def summary(request):
+    context = {}
+    return render(request, 'main/summary.html', context)
 
 
 def price(request):
@@ -39,17 +32,11 @@ def indicator(request):
     return render(request, 'main/indicator.html', context)
 
 
-def daily(request):
-    req_json = request.GET.dict()
-    code = req_json.get("code")
-    url = consts.URL_BODY_CRAWLER + "/api/daily"
-    url += '?code=' + code
-    daily_price = requests.get(url)
-
+def crawl(request):
     context = {}
-    return render(request, 'main/daily.html', context)
+    return render(request, 'main/crawl.html', context)
 
 
-def summary(request):
+def trade(request):
     context = {}
-    return render(request, 'main/summary.html', context)
+    return render(request, 'main/trade.html', context)
